856c9dc907f9a9f2d3e344856f149365f6e4a672
Nimesh Mihiranga <nimeshmihiranga.doc@gmail.com>
2026-04-22 22:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const { MongoClient } = require('mongodb');
 const config  = require('./config');
 
 // ─── ENV ──────────────────────────────────────────────────────────────────────
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://neni:w2jUCfhl2DeTS3is@cluster0.d7uhe8y.mongodb.net/
-';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://neni:w2jUCfhl2DeTS3is@cluster0.d7uhe8y.mongodb.net/';
 const PORT        = process.env.PORT || 8002;
 
 // ─── MongoDB clients ──────────────────────────────────────────────────────────
